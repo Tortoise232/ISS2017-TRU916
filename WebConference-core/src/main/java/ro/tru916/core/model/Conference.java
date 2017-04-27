7a542269cb441b940f68b465faf1758fc1dc05ad
catablack <catalin_pop12@yahoo.com>
2017-04-27 22:50:07 +0300

    Model with User, Paper and Conference

---
+++ WebConference-core/src/main/java/ro/tru916/core/model/Conference.java
@@ -0,0 +1,130 @@
+package ro.tru916.core.model;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by cata on 27.04.2017.
+ */
+@Entity
+@Table(name="conference")
+public class Conference extends BaseEntity<Long> {
+
+    @Column(name="name",nullable = false)
+    private String name;
+
+    @Column(name = "date",nullable = false)
+    private Date date;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    private Set<Paper> papers;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    private Set<User> reviewers;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    private Set<User> speakers;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    private Set<Paper> acceptedpapers;
+
+    public Conference() {
+    }
+
+    public Conference(String name, Date date) {
+        this.name = name;
+        this.date = date;
+        papers=new HashSet<>();
+        reviewers=new HashSet<>();
+        speakers=new HashSet<>();
+        acceptedpapers=new HashSet<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Set<Paper> getPapers() {
+        return papers;
+    }
+
+    public void setPapers(Set<Paper> papers) {
+        this.papers = papers;
+    }
+
+    public Set<User> getReviewers() {
+        return reviewers;
+    }
+
+    public void setReviewers(Set<User> reviewers) {
+        this.reviewers = reviewers;
+    }
+
+    public Set<User> getSpeakers() {
+        return speakers;
+    }
+
+    public void setSpeakers(Set<User> speakers) {
+        this.speakers = speakers;
+    }
+
+    public Set<Paper> getAcceptedpapers() {
+        return acceptedpapers;
+    }
+
+    public void setAcceptedpapers(Set<Paper> acceptedpapers) {
+        this.acceptedpapers = acceptedpapers;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Conference that = (Conference) o;
+
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+        if (date != null ? !date.equals(that.date) : that.date != null) return false;
+        if (papers != null ? !papers.equals(that.papers) : that.papers != null) return false;
+        if (reviewers != null ? !reviewers.equals(that.reviewers) : that.reviewers != null) return false;
+        if (speakers != null ? !speakers.equals(that.speakers) : that.speakers != null) return false;
+        return acceptedpapers != null ? acceptedpapers.equals(that.acceptedpapers) : that.acceptedpapers == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (date != null ? date.hashCode() : 0);
+        result = 31 * result + (papers != null ? papers.hashCode() : 0);
+        result = 31 * result + (reviewers != null ? reviewers.hashCode() : 0);
+        result = 31 * result + (speakers != null ? speakers.hashCode() : 0);
+        result = 31 * result + (acceptedpapers != null ? acceptedpapers.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Conference{" +
+                "name='" + name + '\'' +
+                ", date=" + date +
+                ", papers=" + papers +
+                ", reviewers=" + reviewers +
+                ", speakers=" + speakers +
+                ", acceptedpapers=" + acceptedpapers +
+                '}';
+    }
+}
---
+++ WebConference-core/src/main/java/ro/tru916/core/model/Paper.java
@@ -0,0 +1,117 @@
+package ro.tru916.core.model;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by cata on 27.04.2017.
+ */
+@Entity
+@Table(name="paper")
+public class Paper extends BaseEntity<Long> {
+
+    @Column(name="name",nullable = false)
+    private String name;
+
+    @Column(name="grade")
+    private Float grade;
+
+    @Column(name="path",nullable = false)
+    private String path;
+
+    @Column(name="eventid",nullable = false)
+    private Long evetid;
+
+    @Id
+    @ManyToMany(fetch = FetchType.LAZY)
+    private Set<User> owners;
+
+    public Paper() {
+    }
+
+
+    public Paper(String name, Float grade, String path, Long evetid) {
+        this.name = name;
+        this.grade = grade;
+        this.path = path;
+        this.evetid = evetid;
+        this.owners = new HashSet<User>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<User> getOwners() {
+        return owners;
+    }
+
+    public void setOwners(Set<User> owners) {
+        this.owners = owners;
+    }
+
+    public Float getGrade() {
+        return grade;
+    }
+
+    public void setGrade(Float grade) {
+        this.grade = grade;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public Long getEvetid() {
+        return evetid;
+    }
+
+    public void setEvetid(Long evetid) {
+        this.evetid = evetid;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Paper paper = (Paper) o;
+
+        if (name != null ? !name.equals(paper.name) : paper.name != null) return false;
+        if (grade != null ? !grade.equals(paper.grade) : paper.grade != null) return false;
+        if (path != null ? !path.equals(paper.path) : paper.path != null) return false;
+        if (evetid != null ? !evetid.equals(paper.evetid) : paper.evetid != null) return false;
+        return owners != null ? owners.equals(paper.owners) : paper.owners == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (grade != null ? grade.hashCode() : 0);
+        result = 31 * result + (path != null ? path.hashCode() : 0);
+        result = 31 * result + (evetid != null ? evetid.hashCode() : 0);
+        result = 31 * result + (owners != null ? owners.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Paper{" +
+                "name='" + name + '\'' +
+                ", grade=" + grade +
+                ", path='" + path + '\'' +
+                ", evetid=" + evetid +
+                ", owners=" + owners +
+                '}';
+    }
+}
