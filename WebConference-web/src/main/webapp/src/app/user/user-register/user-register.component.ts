60d3fe56f9673e97fa8180993b544ce9fb5d7d12
Andrei <apple.andrei@gmail.com>
2017-05-11 18:04:01 +0300

    Login & User Register & Conf Register

--- WebConference-web/src/main/webapp/src/app/user/user-register/user-register.component.ts
+++ WebConference-web/src/main/webapp/src/app/user/user-register/user-register.component.ts
@@ -41,36 +41,36 @@ export class RegisterUserComponent {
   register(name, password, username, email): void {
     console.log("register");
 
-    let required = document.getElementById("required");
-    let invalidEmail = document.getElementById("invalid-email");
-    let invalidPassword = document.getElementById("invalid-password");
-    let success = document.getElementById("success");
-    let failure = document.getElementById("failure");
-    required.style.display = "none";
-    invalidEmail.style.display = "none";
-    invalidPassword.style.display = "none";
-    success.style.display = "none";
-    failure.style.display = "none";
-
-    if (!name || !password || !username || !email) {
-      console.log("All fields are required!");
-      required.style.display = "block";
-      return;
-    }
-
-    let emailPattern = /\S+@\S+\.\S+/;
-    if(!emailPattern.test(email)) {
-      console.log("Invalid email!");
-      invalidEmail.style.display = "block";
-      return;
-    }
-
-    let minPasswordLength = 6;
-    if(password.length < minPasswordLength){
-      console.log("Invalid password!");
-      invalidPassword.style.display = "block";
-      return;
-    }
+    // let required = document.getElementById("required");
+    // let invalidEmail = document.getElementById("invalid-email");
+    // let invalidPassword = document.getElementById("invalid-password");
+    // let success = document.getElementById("success");
+    // let failure = document.getElementById("failure");
+    // required.style.display = "none";
+    // invalidEmail.style.display = "none";
+    // invalidPassword.style.display = "none";
+    // success.style.display = "none";
+    // failure.style.display = "none";
+    //
+    // if (!name || !password || !username || !email) {
+    //   console.log("All fields are required!");
+    //   required.style.display = "block";
+    //   return;
+    // }
+    //
+    // let emailPattern = /\S+@\S+\.\S+/;
+    // if(!emailPattern.test(email)) {
+    //   console.log("Invalid email!");
+    //   invalidEmail.style.display = "block";
+    //   return;
+    // }
+    //
+    // let minPasswordLength = 6;
+    // if(password.length < minPasswordLength){
+    //   console.log("Invalid password!");
+    //   invalidPassword.style.display = "block";
+    //   return;
+    // }
 
     let encodedPassword = btoa(password);
     this.userService.register(name, encodedPassword, username, email)
